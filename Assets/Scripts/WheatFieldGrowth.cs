18894b4287bd0d5338894edbb353ece3c0f8d311
Tristan Brumagin <brumagint@gmail.com>
2019-08-24 12:30:38 -0400

    Field Growing Test - Number keys

--- Assets/Scripts/WheatFieldGrowth.cs
+++ Assets/Scripts/WheatFieldGrowth.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 
 public class WheatFieldGrowth : MonoBehaviour
 {
-    GameObject[] Plots;
+    WheatTile[] Plots = null;
 
     [SerializeField]
     MeshFilter Filter;
 
     [SerializeField]
-    Mesh[] States;
+    Mesh[] States = null;
 
-    public const int StatesNumber = 4;
+    public int NumberOfStates { get { return States.Length; } }
     private int _growthState = 0;
 
     void Start()
     {
+        Plots = GetComponentsInChildren<WheatTile>();
         SetState(_growthState);
     }
 
     void Update()
     {
         //For Testing, set state with number keys
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetState(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetState(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetState(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetState(3);
+        }
     }
 
     public void SetState(int state)
     {
-        if (States.Length > state)
+        if (NumberOfStates > state)
         {
+            _growthState = state;
             for (int index = 0; index < Plots.Length; index++) {
-                Plots[index].GetComponent<MeshFilter>().mesh = States[state];
+                Plots[index].UpdateMesh(state-1);
             }
         }
     }
--- Assets/Scripts/WheatTile.cs
+++ Assets/Scripts/WheatTile.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class WheatTile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    MeshFilter SoilMesh = null;
+    [SerializeField]
+    MeshFilter WheatMesh = null;
+    [SerializeField]
+    Mesh[] WheatMeshList = null;
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateMesh(int Stage)
     {
-        
+        //Debug.Log("UpdateMesh: " + Stage);
+        if(Stage < 0)
+        {
+            WheatMesh.mesh = null;
+        }
+        else if( Stage < WheatMeshList.Length)
+        {
+            //Debug.Log("WheatMesh: " + WheatMesh);
+            //Debug.Log("WheatMeshList: " + Stage + " | " + WheatMeshList[Stage].name);
+            WheatMesh.mesh = WheatMeshList[Stage];
+        }
     }
 }
