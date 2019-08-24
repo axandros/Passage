45e5c2c6a16ba9ad286bd6cd4971d703165d904b
Tristan Brumagin <brumagint@gmail.com>
2019-08-24 10:36:07 -0400

    Updated models, created gameplay scene, and started scripts to control fields.

---
+++ Assets/Scripts/WheatFieldGrowth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheatFieldGrowth : MonoBehaviour
+{
+    GameObject[] Plots;
+
+    [SerializeField]
+    MeshFilter Filter;
+
+    [SerializeField]
+    Mesh[] States;
+
+    public const int StatesNumber = 4;
+    private int _growthState = 0;
+
+    void Start()
+    {
+        SetState(_growthState);
+    }
+
+    void Update()
+    {
+        //For Testing, set state with number keys
+    }
+
+    public void SetState(int state)
+    {
+        if (States.Length > state)
+        {
+            for (int index = 0; index < Plots.Length; index++) {
+                Plots[index].GetComponent<MeshFilter>().mesh = States[state];
+            }
+        }
+    }
+}
